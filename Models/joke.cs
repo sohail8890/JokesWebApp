b9660b69a62be3a096c081da25d0aadd291b5dfa
Sohail bhati <bhati889066@gmail.com>
2021-07-05 22:24:50 +0530

    first commit

---
+++ Models/joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesApp.Models
+{
+    public class joke
+    {
+        public int Id { get; set; }
+        public string jokeQuestion { get; set; }
+        public string jokeAnswer { get; set; }
+
+        public joke()
+        {
+
+        }
+    }
+}
